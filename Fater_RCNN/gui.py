ea0e87ee4b42a97e198f83ed4e92d68b9147bd44
ValeriaIvanova <62417917+ValeriaIvanova@users.noreply.github.com>
2023-06-13 00:23:41 +0300

    Update gui.py

--- Fater_RCNN/gui.py
+++ Fater_RCNN/gui.py
@@ -1,221 +1,215 @@
+import os
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QProgressBar, QFileDialog
-from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt, QThread, pyqtSignal
+from PyQt5.QtGui import QPixmap, QImage
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QWidget, QLabel,
+                             QVBoxLayout, QHBoxLayout, QPushButton,
+                             QFileDialog, QProgressBar)
+import cv2
+import numpy as np
+import torch
+import yaml
+from models.create_fasterrcnn_model import create_model
+from utils.annotations import metall_annotations
+from utils.general import set_infer_dir
+from utils.transforms import infer_transforms
+from testing import collect_all_images, parse_opt
+
 
 class ImageProcessor(QThread):
-    progress_update = pyqtSignal(int)
+    finished = pyqtSignal()
 
-    def __init__(self, parent=None):
+    def __init__(self, device, classes, parent=None):
         super().__init__(parent)
-        self.image_path = ""
+        self.image_path = None
+        self.model = None
+        self.threshold = None
+        self.device = device
+        self.classes = classes
 
-    def set_image_path(self, path):
-        self.image_path = path
+    def set_image_path(self, image_path):
+        self.image_path = image_path
+
+    def set_model(self, model):
+        self.model = model
+
+    def set_threshold(self, threshold):
+        self.threshold = threshold
 
     def run(self):
-        for i in range(101):
-            self.progress_update.emit(i)
-            self.msleep(50)
+        if self.image_path is not None and self.model is not None:
+            image = cv2.imread(self.image_path)
+            orig_image = image.copy()
+            # BGR to RGB
+            image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2GRAY)
+            image = infer_transforms(image)
+            # Add batch dimension.
+            image = torch.unsqueeze(image, 0)
+            with torch.no_grad():
+                outputs = self.model(image.to(self.device))
+            # Load all detection to CPU for further operations.
+            outputs = [{k: v.to('cpu') for k, v in t.items()} for t in outputs]
+            COLORS = np.random.uniform(0, 255, size=(len(self.classes), 3))
+            orig_image = metall_annotations(outputs, self.threshold, self.classes, COLORS, orig_image)
+            self.output_image = orig_image
+        else:
+            self.output_image = None
+        self.finished.emit()
 
-class DefectoscopyGUI(QWidget):
-    def __init__(self):
+
+class MainWindow(QMainWindow):
+    def __init__(self, args):
         super().__init__()
-        self.initUI()
+        self.setGeometry(0, 0, 1280, 720)
+        self.setWindowTitle('Дефектоскопия')
 
-    def initUI(self):
-        self.setWindowTitle("Дефектоскопия")
-        self.setGeometry(200, 200, 1280, 720)
+        # Загрузка данных конфигураций
+        data_configs = None
+        if args['config'] is not None:
+            with open(args['config']) as file:
+                data_configs = yaml.safe_load(file)
+            NUM_CLASSES = data_configs['NC']
+            self.CLASSES = data_configs['CLASSES']
 
-        self.load_button = QPushButton(self)
-        self.load_button.setText("Загрузить")
-        self.load_button.move(200, 200)
-        self.load_button.setFixedSize(200, 50)
-        self.load_button.setStyleSheet("background-color: green; border-radius: 25px;")
+        self.DEVICE = args['device']
+        self.OUT_DIR = set_infer_dir()
+
+        # Загрузка предварительно обученной модели
+
+        if args['weights'] is None:
+            # Если конфигурационный файл по-прежнему отсутствует,
+            # то загрузите файл весов для COCO.
+            if data_configs is None:
+                with open(os.path.join('data_configs', 'test_image.yaml')) as file:
+                    data_configs = yaml.safe_load(file)
+                NUM_CLASSES = data_configs['NC']
+                self.CLASSES = data_configs['CLASSES']
+            try:
+                build_model = create_model[args['model']]
+            except:
+                build_model = create_model['fasterrcnn_resnet50_fpn_v2']
+            self.model = build_model(num_classes=NUM_CLASSES, coco_model=True)
+        # Загрузка весов, если путь указан
+        if args['weights'] is not None:
+            checkpoint = torch.load(args['weights'], map_location=self.DEVICE)
+            # If config file is not given, load from model dictionary.
+            if data_configs is None:
+                data_configs = True
+                NUM_CLASSES = checkpoint['config']['NC']
+                self.CLASSES = checkpoint['config']['CLASSES']
+            try:
+                print('Building from model name arguments...')
+                build_model = create_model[str(args['model'])]
+            except:
+                build_model = create_model[checkpoint['model_name']]
+            self.model = build_model(num_classes=NUM_CLASSES, coco_model=False)
+            self.model.load_state_dict(checkpoint['model_state_dict'])
+        self.model.to(self.DEVICE).eval()
+
+        self.threshold = 0.3
+        self.input_image = None
+        self.output_image = None
+
+        # Создание кнопок
+        self.load_button = QPushButton(self, text="Загрузить")
+        font = self.load_button.font()
+        font.setPointSize(14)
+        self.load_button.setFont(font)
         self.load_button.clicked.connect(self.load_image)
 
-
-        self.process_button = QPushButton(self)
-        self.process_button.setText("Обработать")
-        self.process_button.move(450, 200)
-        self.process_button.setFixedSize(200, 50)
-        self.process_button.setStyleSheet("background-color: yellow; border-radius: 25px;")
+        self.process_button = QPushButton(self, text="Обработать")
+        font = self.process_button.font()
+        font.setPointSize(14)
+        self.process_button.setFont(font)
         self.process_button.clicked.connect(self.process_image)
 
-        self.progress_bar = QProgressBar(self)
-        self.progress_bar.move(700, 200)
-        self.progress_bar.setFixedSize(200, 50)
+        # Создание QLabel для исходного изображения
+        self.input_image_label = QLabel()
+        self.input_image_label.setAlignment(Qt.AlignCenter)
 
-        self.show_button = QPushButton(self)
-        self.show_button.setText("Результат")
-        self.show_button.move(900, 200)
-        self.show_button.setFixedSize(200, 50)
-        self.show_button.setStyleSheet("background-color: orange; border-radius: 25px;")
-        self.show_button.clicked.connect(self.show_image)
+        # Создание QLabel для размеченного изображения
+        self.output_image_label = QLabel()
+        self.output_image_label.setAlignment(Qt.AlignCenter)
 
-        self.image_label = QLabel(self)
-        self.image_label.move(200, 400)
-        self.image_label.setFixedSize(400, 200)
+        # Создание прогресс-бара
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setRange(0, 100)
 
-        self.process_label = QLabel(self)
-        self.process_label.move(900, 400)
-        self.process_label.setFixedSize(400, 200)
+        # Создание layout и добавление виджетов
+        left_layout = QVBoxLayout()
+        left_layout.addWidget(self.input_image_label)
+        left_layout.addWidget(self.load_button)
 
-        self.image_processor = ImageProcessor()
-        self.image_processor.progress_update.connect(self.update_progress_bar)
+        right_layout = QVBoxLayout()
+        right_layout.addWidget(self.output_image_label)
+        right_layout.addWidget(self.process_button)
+
+        centr_layout = QVBoxLayout()
+        centr_layout.addWidget((self.progress_bar))
+
+        main_layout = QHBoxLayout()
+        main_layout.setSpacing(150)
+        main_layout.setContentsMargins(100, 250, 250, 100)
+
+        main_layout.addLayout(left_layout)
+        main_layout.addLayout(centr_layout)
+        main_layout.addLayout(right_layout)
+
+        central_widget = QWidget()
+        central_widget.setLayout(main_layout)
+
+        self.setCentralWidget(central_widget)
+
+        # стиль кнопок
+        self.load_button.setStyleSheet('background-color: green; width: 250px; height: 50px; border-radius: 15px')
+        self.process_button.setStyleSheet("background-color: yellow; width: 250px; height: 50px; border-radius: 15px")
+        self.progress_bar.setStyleSheet("width: 250px; height: 50px; border-radius: 15px")
+
 
     def load_image(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        file_name, _ = QFileDialog.getOpenFileName(self, "Выберите изображение", "", "Images (*.png *.xpm *.jpg *.bmp *.gif)", options=options)
-        if file_name:
-            self.image_path = file_name
-            pixmap = QPixmap(file_name)
-            self.image_label.setPixmap(pixmap.scaled(self.image_label.width(), self.image_label.height(), Qt.KeepAspectRatio))
+        self.file_name, _ = QFileDialog.getOpenFileName(self, 'Open Image', '.', 'Image Files (*.png *.jpg *.jpeg)')
+        if self.file_name:
+            self.input_image = QImage(self.file_name)
+            pixmap = QPixmap.fromImage(self.input_image)
+            self.input_image_label.setPixmap(pixmap.scaled(400, 400, aspectRatioMode=Qt.KeepAspectRatio))
+
 
     def process_image(self):
-        self.image_processor.set_image_path(self.image_path)
+        if not self.input_image:
+            return
+
+        self.process_button.setEnabled(False)
+        self.progress_bar.setValue(0)
+
+        self.image_processor = ImageProcessor(self.DEVICE, self.CLASSES)
+        self.image_processor.set_image_path(self.file_name)
+        self.image_processor.set_model(self.model)
+        self.image_processor.set_threshold(self.threshold)
+        self.image_processor.finished.connect(self.update_image)
         self.image_processor.start()
 
-    def update_progress_bar(self, value):
-        self.progress_bar.setValue(value)
+    def update_image(self):
+        if self.image_processor.output_image is not None:  # Проверяем, что было получено распознанное изображение
+            output_image = self.image_processor.output_image  # Получаем распознанное изображение
+            h, w, c = output_image.shape
+            q_output_image = QImage(output_image.data, w, h, c * w,
+                                    QImage.Format_RGB888)  # Конвертируем изображение из numpy array в QImage
+            pixmap = QPixmap.fromImage(q_output_image)
+            self.output_image_label.setPixmap(
+                pixmap.scaled(400, 400, aspectRatioMode=Qt.KeepAspectRatio))  # Устанавливаем распознанное изображение на QLabel, чтобы отобразить его на форме
+        self.process_button.setEnabled(True)  # Включаем кнопку "Process Image"
+        self.progress_bar.setValue(100)  # Устанавливаем значение прогресс-бара на 100%
+        self.progress_bar.setStyleSheet("width: 250px; height: 50px; border-radius: 15px")
 
-    def show_image(self):
-        pixmap = QPixmap(self.image_path)
-        self.process_label.setPixmap(pixmap.scaled(self.process_label.width(), self.process_label.height(), Qt.KeepAspectRatio))
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        file_name, _ = QFileDialog.getOpenFileName(self, "Выберите изображение", "",
-                                                   "Images (*.png *.xpm *.jpg *.bmp *.gif)", options=options)
-        if file_name:
-            self.image_path = file_name
-            pixmap = QPixmap(file_name)
-            self.image_label.setPixmap(
-                pixmap.scaled(self.image_label.width(), self.image_label.height(), Qt.KeepAspectRatio))
+    def closeEvent(self, event):
+        if hasattr(self, 'image_processor'):
+            self.image_processor.quit()
+            self.image_processor.wait()
+            event.accept()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    gui = DefectoscopyGUI()
-    gui.show()
+    main_window = MainWindow(parse_opt())
+    main_window.show()
     sys.exit(app.exec_())
-
-
-
-
-'''КОД С ВКЛЮЧЕНИЕМ МОДЕЛИ'''
-#
-# import sys
-# from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QPushButton, QFileDialog, QLineEdit
-# import torch
-# import torchvision.transforms as transforms
-# import argparse
-#
-# # загрузка обученной модели
-# model = torch.load('model.pth')
-#
-# # преобразование изображения
-# transform = transforms.Compose([
-#     transforms.Resize(256),
-#     transforms.CenterCrop(224),
-#     transforms.ToTensor(),
-#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-# ])
-#
-# # класс для обработки изображения
-# class ImageProcessor:
-#     def __init__(self):
-#         self.image = None
-#         self.result = None
-#
-#     # функция для загрузки изображения
-#     def load_image(self, filename):
-#         self.image = QPixmap(filename)
-#
-#     # функция для обработки изображения
-#     def process_image(self, threshold):
-#         if self.image:
-#             # обработка изображения
-#             image = self.image.toImage()
-#             image_tensor = transform(image).unsqueeze(0)
-#             with torch.no_grad():
-#                 output = model(image_tensor)
-#             result = output.argmax(dim=1).item()
-#
-#             # создание изображения с результатом
-#             self.result = QPixmap(self.image.size())
-#             self.result.fill(Qt.transparent)
-#             painter = QPainter(self.result)
-#             painter.drawPixmap(0, 0, self.image)
-#             painter.setPen(QPen(Qt.red, 2))
-#             painter.drawRect(10, 10, 100, 100)
-#             painter.setPen(QPen(Qt.green, 2))
-#             painter.drawText(10, 30, 'Класс: {}'.format(result))
-#             painter.end()
-#
-#     # функция для показа результата
-#     def show_result(self):
-#         if self.result:
-#             return self.result
-#
-# # класс для создания интерфейса
-# class MainWindow(QMainWindow):
-#     def __init__(self):
-#         super().__init__()
-#
-#         # создание кнопок
-#         self.load_button = QPushButton(self)
-#         self.load_button.setGeometry(10, 10, 100, 30)
-#         self.load_button.setStyleSheet('background-color: red; border-radius: 15px;')
-#         self.load_button.clicked.connect(self.load_image)
-#
-#         self.process_button = QPushButton(self)
-#         self.process_button.setGeometry(10, 50, 100, 30)
-#         self.process_button.setStyleSheet('background-color: yellow; border-radius: 15px;')
-#         self.process_button.clicked.connect(self.process_image)
-#
-#         self.show_button = QPushButton(self)
-#         self.show_button.setGeometry(10, 90, 100, 30)
-#         self.show_button.setStyleSheet('background-color: green; border-radius: 15px;')
-#         self.show_button.clicked.connect(self.show_result)
-#
-#         # создание полей ввода
-#         self.input_edit = QLineEdit(self)
-#         self.input_edit.setGeometry(120, 10, 256, 30)
-#
-#         self.threshold_edit = QLineEdit(self)
-#         self.threshold_edit.setGeometry(120, 50, 256, 30)
-#
-#         # создание изображений
-#         self.image_label = QLabel(self)
-#         self.image_label.setGeometry(400, 10, 256, 256)
-#
-#         self.result_label = QLabel(self)
-#         self.result_label.setGeometry(700, 10, 256, 256)
-#
-#         # инициализация переменных
-#         self.image_processor = ImageProcessor()
-#
-#     # функция для загрузки изображения
-#     def load_image(self):
-#         filename, _ = QFileDialog.getOpenFileName(self, 'Выбрать изображение', '', 'Images (*.png *.xpm *.jpg *.bmp)')
-#         if filename:
-#             self.input_edit.setText(filename)
-#             self.image_processor.load_image(filename)
-#             self.image_label.setPixmap(self.image_processor.image)
-#
-#     # функция для обработки изображения
-#     def process_image(self):
-#         threshold = float(self.threshold_edit.text())
-#         self.image_processor.process_image(threshold)
-#         self.result_label.setPixmap(self.image_processor.result)
-#
-#     # функция для показа результата
-#     def show_result(self):
-#         self.result_label.setPixmap(self.image_processor.show_result())
-#
-# if __name__ == '__main__':
-#     app = QApplication(sys.argv)
-#     window = MainWindow()
-#     window.setGeometry(100, 100, 1000, 300)
-#     window.show()
-#     sys.exit(app.exec_())
