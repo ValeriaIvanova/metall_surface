78a3ddfb34f1659f3b1c91151e9ffc4e3b0dd6be
ValeriaIvanova <62417917+ValeriaIvanova@users.noreply.github.com>
2023-06-13 00:24:27 +0300

    Update train.py

--- Fater_RCNN/train.py
+++ Fater_RCNN/train.py
@@ -1,23 +1,10 @@
-"""
-USAGE
-
-# Training with Faster RCNN ResNet50 FPN model without mosaic or any other augmentation:
-python train.py --model fasterrcnn_resnet50_fpn --epochs 2 --config data_configs/voc.yaml --no-mosaic --batch-size 4
-
-# Training on ResNet50 FPN with custom project folder name with mosaic augmentation (ON by default):
-python train.py --model fasterrcnn_resnet50_fpn --epochs 2 --config data_configs/voc.yaml --project-name resnet50fpn_voc --batch-size 4
-
-# Training on ResNet50 FPN with custom project folder name with mosaic augmentation (ON by default) and added training augmentations:
-python train.py --model fasterrcnn_resnet50_fpn --epochs 2 --use-train-aug --config data_configs/voc.yaml --project-name resnet50fpn_voc --batch-size 4
-"""
-
 from torch_utils.engine import (
-    train_one_epoch, evaluate
-)
+    train_one_epoch, evaluate)
+
 from datasets import (
     create_train_dataset, create_valid_dataset, 
-    create_train_loader, create_valid_loader
-)
+    create_train_loader, create_valid_loader)
+
 from models.create_fasterrcnn_model import create_model
 from utils.general import (
     set_training_dir, Averager, 
@@ -108,13 +95,14 @@ def parse_opt():
     return args
 
 def main(args):
-    # Initialize W&B with project name.
+    # Инициализация имени проекта
     # wandb_init(name=args['project_name'])
-    # Load the data configurations
+    # Загрузка конфигурации данных
+
     with open(args['config']) as file:
         data_configs = yaml.safe_load(file)
     
-    # Settings/parameters/constants.
+    # Настройки/параметры/константы.
     TRAIN_DIR_IMAGES = data_configs['TRAIN_DIR_IMAGES']
     TRAIN_DIR_LABELS = data_configs['TRAIN_DIR_LABELS']
     VALID_DIR_IMAGES = data_configs['VALID_DIR_IMAGES']
@@ -131,9 +119,9 @@ def main(args):
     COLORS = np.random.uniform(0, 1, size=(len(CLASSES), 3))
     # Set logging file.
     set_log(OUT_DIR)
-    # writer = set_summary_writer(OUT_DIR)
 
-    # Model configurations
+
+    # Конфигурация модели
     IMAGE_WIDTH = args['img_size']
     IMAGE_HEIGHT = args['img_size']
     
@@ -157,8 +145,10 @@ def main(args):
 
     # Initialize the Averager class.
     train_loss_hist = Averager()
-    # Train and validation loss lists to store loss values of all
-    # iterations till ena and plot graphs for all iterations.
+
+    # Подготавка и проверка функций потерь для хранения значений потерь всех на эпохах
+    # построение графиков
+
     train_loss_list = []
     loss_cls_list = []
     loss_box_reg_list = []
@@ -174,25 +164,25 @@ def main(args):
         build_model = create_model[args['model']]
         model = build_model(num_classes=NUM_CLASSES, pretrained=True)
 
-    # Load pretrained weights if path is provided.
+    # Загрузка предварительно подготовленных весов, если указан путь.
     if args['weights'] is not None:
         print('Loading pretrained weights...')
         
-        # Load the pretrained checkpoint.
+        # Загрузите предварительно подготовленную модели.
         checkpoint = torch.load(args['weights'], map_location=DEVICE) 
         keys = list(checkpoint['model_state_dict'].keys())
         ckpt_state_dict = checkpoint['model_state_dict']
-        # Get the number of classes from the loaded checkpoint.
+        # Получение количества классов из загруженной модели
         old_classes = ckpt_state_dict['roi_heads.box_predictor.cls_score.weight'].shape[0]
 
-        # Build the new model with number of classes same as checkpoint.
+        # Создание новой модели с количеством классов, таким же, как у обученной модели.
         build_model = create_model[args['model']]
         model = build_model(num_classes=old_classes)
-        # Load weights.
+        # Загрузка весов
         model.load_state_dict(ckpt_state_dict)
 
-        # Change output features for class predictor and box predictor
-        # according to current dataset classes.
+        # Изменение выходных функций для class predictor и box predictor
+        # в соответствии с текущими классами набора данных.
         in_features = model.roi_heads.box_predictor.cls_score.in_features
         model.roi_heads.box_predictor.cls_score = torch.nn.Linear(
             in_features=in_features, out_features=NUM_CLASSES, bias=True
@@ -203,8 +193,9 @@ def main(args):
 
         if args['resume_training']:
             print('RESUMING TRAINING...')
-            # Update the starting epochs, the batch-wise loss list, 
-            # and the epoch-wise loss list.
+            # Обновление начальных эпох, the batch-wise списка потерь,
+            # и список потерь по эпохам.
+
             if checkpoint['epoch']:
                 start_epochs = checkpoint['epoch']
                 print(f"Resuming from epoch {start_epochs}...")
@@ -222,25 +213,25 @@ def main(args):
         
     print(model)
     model = model.to(DEVICE)
-    # Total parameters and trainable parameters.
+    # Общие параметры и обучаемые параметры.
     total_params = sum(p.numel() for p in model.parameters())
     print(f"{total_params:,} total parameters.")
     total_trainable_params = sum(
         p.numel() for p in model.parameters() if p.requires_grad)
     print(f"{total_trainable_params:,} training parameters.")
-    # Get the model parameters.
+    # Использование параметров модели
     params = [p for p in model.parameters() if p.requires_grad]
-    # Define the optimizer.
+    # Определение оптимизатора
     optimizer = torch.optim.SGD(params, lr=0.001, momentum=0.9, nesterov=True)
-    # optimizer = torch.optim.AdamW(params, lr=0.0001, weight_decay=0.0005)
+
     if args['resume_training']: 
-        # LOAD THE OPTIMIZER STATE DICTIONARY FROM THE CHECKPOINT.
+        # ЗАГРУЗИТЕ СЛОВАРЬ СОСТОЯНИЙ ОПТИМИЗАТОРА ИЗ КОНТРОЛЬНОЙ ТОЧКИ.
         print('Loading optimizer state dictionary...')
         optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
     if args['cosine_annealing']:
-        # LR will be zero as we approach `steps` number of epochs each time.
-        # If `steps = 5`, LR will slowly reduce to zero every 5 epochs.
+        # LR будет равен нулю по мере того, как мы каждый раз приближаемся к числу "шагов" в эпохе
+        # Если `steps = 5`, LR будет медленно уменьшаться до нуля каждые 5 эпох.
         steps = NUM_EPOCHS + 10
         scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(
             optimizer, 
@@ -281,20 +272,20 @@ def main(args):
             colors=COLORS
         )
 
-        # Append the current epoch's batch-wise losses to the `train_loss_list`.
+        # Добавление потери текущей эпохи по пакетам в `train_loss_list`.
         train_loss_list.extend(batch_loss_list)
         loss_cls_list.extend(batch_loss_cls_list)
         loss_box_reg_list.extend(batch_loss_box_reg_list)
         loss_objectness_list.extend(batch_loss_objectness_list)
         loss_rpn_list.extend(batch_loss_rpn_list)
-        # Append curent epoch's average loss to `train_loss_list_epoch`.
+        # Добавление средней потери текущей эпохи к `train_loss_list_epoch`.
         train_loss_list_epoch.append(train_loss_hist.value)
         val_map_05.append(stats[1])
         val_map.append(stats[0])
 
-        # Save loss plot for batch-wise list.
+        # Сохранение графика потерь для batch-size
         save_loss_plot(OUT_DIR, train_loss_list)
-        # Save loss plot for epoch-wise list.
+        # Сохранение графика потерь для списка эпох
         save_loss_plot(
             OUT_DIR, 
             train_loss_list_epoch,
@@ -331,14 +322,14 @@ def main(args):
             save_name='loss_rpn_bbox'
         )
 
-        # Save mAP plots.
+        # Сохранение графика средней точнсти
         save_mAP(OUT_DIR, val_map_05, val_map)
 
         coco_log(OUT_DIR, stats)
 
-        # Save the current epoch model state. This can be used 
-        # to resume training. It saves model state dict, number of
-        # epochs trained for, optimizer state dict, and loss function.
+        # Сохранение текущего состояние модели эпохи. Можно использовать
+        # для возобновления тренировки. Он сохраняет данные о состоянии модели, количество
+        # обученных эпох, данные о состоянии оптимизатора и функцию потерь.
         save_model(
             epoch, 
             model, 
@@ -351,10 +342,10 @@ def main(args):
             data_configs,
             args['model']
         )
-        # Save the model dictionary only for the current epoch.
+        # Сохранение словаря модели только для текущей эпохи.
         save_model_state(model, OUT_DIR, data_configs, args['model'])
-        # Save best model if the current mAP @0.5:0.95 IoU is
-        # greater than the last hightest.
+        # SСохранение лучшей моделиь, если текущее значение mAP @0.5:0.95 IoU
+        # больше, чем последнее самое высокое значение.
         save_best_model(
             model, 
             val_map[-1], 
